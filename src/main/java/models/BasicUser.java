b0b15250b19eb7f28696865ff015d65e689c77bb
Ekene Attoh <ekeneattoh@MacBook-Pro.local>
2020-07-17 18:36:47 +0200

    added concrete user class

---
+++ src/main/java/models/BasicUser.java
@@ -0,0 +1,9 @@
+package models;
+
+class BasicUser extends User {
+
+    public BasicUser(String email, String password, String firstname, String lastname) {
+        super(email, password, firstname, lastname);
+    }
+}
+
